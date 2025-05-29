092178ac41ba3b61a9abeda663002bd5b4ca0c82
Maaher01 <maaher1145@gmail.com>
2025-05-29 17:08:30 +0600

    Fixed access token jwt sign structure

--- src/middleware/check-user-auth.js
+++ src/middleware/check-user-auth.js
@@ -1,18 +1,16 @@
 const jwt = require("jsonwebtoken");
 
 module.exports = (req, res, next) => {
-  const authHeader = req.get("Authorization");
+  const token = req.cookies.jwt;
 
-  if (!authHeader) {
-    const error = new Error("Sorry! you are not authorized");
+  if (!token) {
+    const error = new Error("Unauthorized: No token provided");
     error.statusCode = 401;
-    throw error;
+    return next(error);
   }
 
-  const token = authHeader.split(" ")[1];
-
   try {
-    const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+    const decodedToken = jwt.verify(token, process.env.REFRESH_TOKEN_SECRET);
 
     req.userData = {
       email: decodedToken.UserInfo.email,
@@ -21,7 +19,8 @@ module.exports = (req, res, next) => {
 
     next();
   } catch (err) {
-    err.statusCode = 500;
-    throw err;
+    err.statusCode = 403;
+    err.message = "Invalid or expired token";
+    next(err);
   }
 };
